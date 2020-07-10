97e47d5a4fc633900d449f1000d67dadc8e1ad3c
DrgRol <dvdrol@gmail.com>
2020-07-10 14:07:47 +0200

    Proyecto Marco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  direccion = '';
   title = 'marco-Orquestador';
+  iframe = false;
+  evento = false;
+
+  constructor(public sanitizer: DomSanitizer){}
+
+  cargar(){
+
+    console.log(this.direccion);
+    this.iframe = true;
+  }
+
+  @HostListener('window:message', ['$event'])
+  onPostMessage(event) {
+    console.log('evento de mensaje recibido: ', event);
+    if (event.data && event.data.name === 'poc') {
+      this.evento = true;
+    }
+    //controlar el origen del evento:
+    /*if(event.origin !== 'https://example.com')
+      return;*/
+  }
 }
